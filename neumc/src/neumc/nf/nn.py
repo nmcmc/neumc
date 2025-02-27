fde0a828f3063990469cb2dc96ed5283fd99eee4
Dawid Zapolski <dzapolski3@gmail.com>
2025-02-27 17:01:25 +0100

    Fix: make_conv_net accepts kernel size as list. Docs: make_conv_net

--- neumc/src/neumc/nf/nn.py
+++ neumc/src/neumc/nf/nn.py
@@ -1,52 +1,80 @@
-# TODO: Add docstring to this file
 import torch
+
 
 def _make_list(k, n):
     if isinstance(k, int):
-        return [k]*n
+        return [k] * n
     else:
         return k
 
+
 def make_conv_net(
     *,
-    in_channels,
-    hidden_channels,
-    out_channels,
-    kernel_size,
-    use_final_tanh,
-    dilation=1,
-    activation=torch.nn.LeakyReLU,
+    in_channels: int,
+    hidden_channels: list,
+    out_channels: int,
+    kernel_size: int | list,
+    use_final_tanh: bool,
+    dilation: int | list = 1,
+    activation: type[torch.nn.Module] = torch.nn.LeakyReLU,
     bias=True,
     float_dtype=torch.float32,
 ):
     """
-    Create a convolutional neural network with given activations and a optional final Tanh activation.
+    Create a convolutional neural network with given activations and an optional final Tanh activation.
 
     Parameters
     ----------
     in_channels
+        number of input parameters for the first network
+
     hidden_channels
+        list of numbers of input parameters for hidden layers
+
     out_channels
+        number of output parameters for the last layer
+
     kernel_size
+        Size of the kernel. If one number is provided, all kernels will have the same size. If the list
+        of numbers is provided, consecutive convolutional layers will have corresponding kernel sizes.
+        The list should have exactly `len(hidden_channels) + 1` elements.
+
     use_final_tanh
+        If true, the output of the last layer will be transformed by `tanh`. In the opposite case, the output
+        of the last layer will be returned without any additional transformation.
+
     dilation
+        Number of rows and columns between consecutive convolution calculations. For 1, no skipping.
+        If it is a number, all layers will have the same dilation. If is is a list, consecutive layers will have
+        corresponding dilations. The list should have exactly `len(hidden_channels) + 1` elements.
+
+    activation
+        Type of activation function to be used. Note that it takes a type, not an instance of a Module. Constructor of the
+        activation function should not take any additional arguments. Default: `LeakyReLU`.
+
+    bias
+        If true, bias is used in all convolutional layers. Default: `True`.
+
     float_dtype
+        Type to be used by convolution.
 
     Returns
     -------
 
+    Sequential with consecutive convolutional layers.
+
     """
     sizes = [in_channels] + hidden_channels + [out_channels]
     net = []
-    dilations = _make_list(dilation, len(sizes)-1)
-    kernel_sizes = _make_list(kernel_size, len(sizes)-1)
+    dilations = _make_list(dilation, len(sizes) - 1)
+    kernel_sizes = _make_list(kernel_size, len(sizes) - 1)
     for i in range(len(sizes) - 1):
         net.append(
             torch.nn.Conv2d(
                 sizes[i],
                 sizes[i + 1],
                 kernel_sizes[i],
-                padding=dilations[i] * (kernel_size - 1) // 2,
+                padding=dilations[i] * (kernel_sizes[i] - 1) // 2,
                 stride=1,
                 padding_mode="circular",
                 dilation=dilations[i],
